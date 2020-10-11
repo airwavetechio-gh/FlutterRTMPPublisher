e9276ec59807979b9f1517bea224c533f0325e19
tonyjchong <tonyjchong@gmail.com>
2020-10-11 12:15:24 -0700

    revert to match_parent, remove back button

--- android/src/main/kotlin/io/github/ponnamkarthik/flutterrtmppublisher/RTMPActivity.kt
+++ android/src/main/kotlin/io/github/ponnamkarthik/flutterrtmppublisher/RTMPActivity.kt
@@ -81,6 +81,7 @@ class RTMPActivity:AppCompatActivity(), RtmpHandler.RtmpListener, SrsRecordHandl
             } else if (btnPublish.text.toString().contains("Stop")) {
                 mPublisher.stopPublish()
                 mPublisher.stopRecord()
+                this.onBackPressed()
                 btnPublish.text = getString(R.string.start_publishing)
 
 //                btnPause.isEnabled = false
